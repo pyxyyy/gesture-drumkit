4d1346e395adf16518566f20524af79d196a742e
Alex Fong <alexfongg@gmail.com>
2019-03-30 13:35:06 +0800

    added top row buttons (no code)

---
+++ AndroidApp/app/src/main/java/com/cs4347/drumkit/GenerateTrackActivity.kt
@@ -0,0 +1,12 @@
+package com.cs4347.drumkit
+
+import android.app.Activity
+import android.os.Bundle
+
+class GenerateTrackActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_track)
+    }
+}
