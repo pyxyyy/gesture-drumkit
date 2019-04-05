e0957d3c2018ce79da80cc3beb5285e23f8c17b3
Alex Fong <alexfongg@gmail.com>
2019-04-05 19:47:08 +0800

    more small adjustments & added up/down tempo

--- AndroidApp/app/src/main/java/com/cs4347/drumkit/GenerateTrackActivity.kt
+++ AndroidApp/app/src/main/java/com/cs4347/drumkit/GenerateTrackActivity.kt
@@ -2,15 +2,19 @@ package com.cs4347.drumkit
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import com.cs4347.drumkit.view.DrumKitInstrumentsAdapter
 import kotlinx.android.synthetic.main.activity_generate_track.*
-import kotlinx.android.synthetic.main.view_drumkit_instruments_view.*
+import kotlin.math.max
+import kotlin.math.min
 
 class GenerateTrackActivity : Activity() {
 
-    private var selectedInstrumentRow = 0
+    companion object {
+        private val tempoRange = Pair(60, 120)
+        private const val tempoStep = 10
+    }
+
     private val instruments = listOf(
             "Kick" to R.color.colorKick,
             "Cymbal" to R.color.colorFingerCymbal,
@@ -22,6 +26,9 @@ class GenerateTrackActivity : Activity() {
     )
     private val instrumentsAdapter = DrumKitInstrumentsAdapter(instruments)
 
+    private var tempo = tempoRange.first
+    private var selectedInstrumentRow = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_track)
@@ -31,5 +38,22 @@ class GenerateTrackActivity : Activity() {
             this.layoutManager = LinearLayoutManager(this@GenerateTrackActivity)
         }
 
+        tempoUp.setOnClickListener {
+            tempo = min(tempoRange.second, tempo + tempoStep)
+            setTempoText()
+        }
+
+        tempoDown.setOnClickListener {
+            tempo = max(tempoRange.first, tempo - tempoStep)
+            setTempoText()
+        }
+
+        setTempoText()
+
     }
+
+    private fun setTempoText() {
+        tempoText.text = resources.getString(R.string.tempo_display, tempo)
+    }
+
 }
